c9658460af81c344f1e9db4019c8446f578f17f1
Aaron Dosser <adosser@ded.zone>
2018-08-13 22:04:51 -0400

    Moisten my biscuits :d

---
+++ components/Koinboss.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
+import CoinView from './CoinView';
+
+class Koinboss extends React.PureComponent {
+    render() {
+        return (
+            <View style={styles.container}>
+            <View style={styles.navbar}>
+                <Text style={styles.logoPlaceholder}>KOINBOSS</Text>
+            </View>
+            <View style={styles.moneyDisplay}>
+                <View style={styles.moneyContainer}>
+                    <Text>${JSON.stringify(this.props)}</Text>
+                </View>
+                <View style={styles.separator} />
+                <View style={styles.moneyContainer} />
+            </View>
+            <View style={styles.horizSeparator} />
+            <ScrollView style={styles.scroll}>
+                <CoinView
+                coin={{
+                    name: 'BlocKoin',
+                    img: require('../resources/coin-logos/BlocKoinLogO.png'),
+                    colors: ['#f9ba2b', '#0842b3']
+                }}/>
+            </ScrollView>
+            </View>
+        );
+    }
+}
+
+export default connect(state => ({ ...state }))(Koinboss);
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      flexDirection: 'column',
+      backgroundColor: '#d9e8d6',
+      alignItems: 'center',
+      justifyContent: 'flex-start',
+    },
+    navbar: {
+      width: '100%',
+      height: 50,
+      justifyContent: 'center',
+      backgroundColor: '#3d9645'
+    },
+    scroll: {
+      flex: 1,
+      height: '100%',
+      width: '100%'
+    },
+    logoPlaceholder: {
+      fontSize: 24,
+      color: '#d9e8d6',
+      paddingLeft: 15
+    },
+    moneyDisplay: {
+      height: 100,
+      width: '100%',
+      flexDirection: 'row',
+      padding: 8,
+      paddingBottom: 5,
+      justifyContent: 'space-around'
+    },
+    moneyContainer: {
+      height: '100%',
+      borderRadius: 5,
+      width: '49%',
+      backgroundColor: '#fff'
+    },
+    separator: {
+      height: '100%',
+      width: 2,
+      marginLeft: 5,
+      marginRight: 5,
+      backgroundColor: '#6c966c'
+    },
+    horizSeparator: {
+      height: 2,
+      width: '96%',
+      backgroundColor: '#6c966c'
+    }
+  });
--- ducks/index.js
+++
@@ -1,4 +0,0 @@
-import { createStore } from 'redux';
-import gameState from './gameState';
-
-export default createStore(gameState);
